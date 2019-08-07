bea22c38dede8f291b15d77d464c81c1e4be069a
Yelyzaveta <elizaveta.kzm@gmail.com>
2019-08-07 17:27:23 +0300

    Updates

---
+++ precache-manifest.13d78a4c6119663cac6e4b60ffaf6a5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d964b34446c9a910cc9432bc288a0cdf",
+    "url": "/typing-trainer/index.html"
+  },
+  {
+    "revision": "e3639652519d673e1716",
+    "url": "/typing-trainer/static/css/main.24047d79.chunk.css"
+  },
+  {
+    "revision": "f85be7531fa004991813",
+    "url": "/typing-trainer/static/js/2.3c2a96b6.chunk.js"
+  },
+  {
+    "revision": "e3639652519d673e1716",
+    "url": "/typing-trainer/static/js/main.bfb3c526.chunk.js"
+  },
+  {
+    "revision": "42bff806f6f5e34fc469",
+    "url": "/typing-trainer/static/js/runtime~main.57030ce5.js"
+  },
+  {
+    "revision": "93a36e319ae61b91338e83e9471fa1a6",
+    "url": "/typing-trainer/static/media/mop.93a36e31.svg"
+  },
+  {
+    "revision": "524e3a4d4a5a0fc834ce19ab75bf9a33",
+    "url": "/typing-trainer/static/media/water.524e3a4d.gif"
+  }
+]);
